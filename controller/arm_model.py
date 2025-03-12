3cc4eb0b4aaa07789b8596b203e05d72b12717c4
OHeckIsThatSam <sam@scrooby.net>
2025-03-12 16:55:04 +0000

    Added vex project settings

--- controller/arm_model.py
+++ controller/arm_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import pi
 from visual_kinematics.RobotSerial import *
 
 class ArmModel():
---
+++ controller/main.py
@@ -0,0 +1,10 @@
+from arm_model import ArmModel
+
+
+def main():
+    model = ArmModel()
+
+    
+
+if __name__ == "main":
+    main()
