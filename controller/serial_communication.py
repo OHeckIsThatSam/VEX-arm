0bff6130819dcfb9b4057f702ec34e4f3832ea6c
OHeckIsThatSam <sam@scrooby.net>
2025-03-12 16:34:08 +0000

    Add inverse kinematics for end manipulator positioning

--- controller/serial_communication.py
+++ controller/serial_communication.py
@@ -1,6 +1,10 @@
 import serial
 from time import sleep
+
+# COM4 for wireless connection with controller
+# COM6 for wired connection with VEX brain
 ser = serial.Serial('COM4', 115200)
 while True:
+    # Temp test data written
     ser.write("d".encode())
     sleep(0.01)
