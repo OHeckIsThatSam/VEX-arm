79773b9e1d71559c3a8b9d74154a85bc99a1aad1
OHeckIsThatSam <sam@scrooby.net>
2025-03-12 17:40:21 +0000

    comm demo

--- controller/main.py
+++ controller/main.py
@@ -1,5 +1,7 @@
 from arm_model import ArmModel
+import serial_communication
 import numpy as np
+
 
 def main():
     model = ArmModel()
@@ -9,7 +11,11 @@ def main():
     z = 0.3
     
     results = model.calc_joint_degrees(x, y, z)
+    
+    data = f"{round(results[1], 1)} {round(results[2], 1)} {round(results[3],1)}"
 
+    serial_communication.send_data(data)
+    
     print(np.array2string(results))
     model.show()
 
--- controller/serial_communication.py
+++ controller/serial_communication.py
@@ -1,10 +1,10 @@
 import serial
-from time import sleep
 
-# COM4 for wireless connection with controller
-# COM6 for wired connection with VEX brain
-ser = serial.Serial('COM4', 115200)
-while True:
+
+def send_data(data):
+    data += "\r\n"
+    # COM4 for wireless connection with controller
+    # COM6 for wired connection with VEX brain
+    ser = serial.Serial('COM4', 115200)
     # Temp test data written
-    ser.write("d".encode())
-    sleep(0.01)
+    ser.write(data.encode())
