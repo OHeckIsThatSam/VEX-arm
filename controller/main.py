0ad2853432d8c6f0c73fd86a6aa385cc1d11560d
OHeckIsThatSam <sam@scrooby.net>
2025-03-12 17:12:33 +0000

    demo test

--- controller/main.py
+++ controller/main.py
@@ -1,10 +1,18 @@
 from arm_model import ArmModel
-
+import numpy as np
 
 def main():
     model = ArmModel()
 
+    x = 0.2
+    y = 0.2
+    z = 0.3
     
+    results = model.calc_joint_degrees(x, y, z)
 
-if __name__ == "main":
+    print(np.array2string(results))
+    model.show()
+
+
+if __name__ == "__main__":
     main()
